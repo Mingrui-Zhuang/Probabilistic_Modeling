92619d93ef77dc24824b0020a1ab56db4eedfb8e
Mingrui-Zhuang <m1zhuang@ucsd.edu>
2025-04-04 14:42:02 -0700

    Apr 4

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('find-dataset').addEventListener('click', function () {
+    window.location.href = 'https://datasetsearch.research.google.com'; // Replace with your URL
+});
